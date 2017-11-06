b41fbd596e71a4fa66d902af697910aac1713f93
mcwarman <mcwarman@gmail.com>
2017-11-06 20:19:39 +0000

    Remove echo from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = 'master' ]; then
   PROFILES="$PROFILES,sign"
 fi
 if [[ $TRAVIS_BRANCH =~ ^master|develop$ ]] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  echo mvn deploy -P$PROFILES -DskipTests=true -Dinvoker.skip=true -Djacoco.skip=true --settings .travis-m2-settings.xml -B
+  mvn deploy -P$PROFILES -DskipTests=true -Dinvoker.skip=true -Djacoco.skip=true --settings .travis-m2-settings.xml -B
 fi
